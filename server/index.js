4211104bd61607a4a7e01603b3b1b86181c02462
Julie Pearl <ayanosuzuki04@gmail.com>
2026-06-07 22:21:32 +0800

    Move package.json and lock into server/
    Relocate npm config to the server directory: remove root package.json and package-lock.json and add server/package.json and server/package-lock.json. This reorganizes the repo so Node deps and scripts live under server/ (e.g. to separate backend); run npm install in server/ after pulling these changes.

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.json({ ok: true }));
+
+app.listen(3001, () => console.log('API running on http://localhost:3001'));
